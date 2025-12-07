369d7a69ea651d3aabe253953fc089c377ea6b8e
GAITH SLMAN <gaithslman7@gmail.com>
2025-12-07 09:15:59 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/home";
+import Setup from "@/pages/setup";
+import Game from "@/pages/game";
+import GameBuzzer from "@/pages/game-buzzer";
+import GameAuction from "@/pages/game-auction";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/setup" component={Setup} />
+      <Route path="/game" component={Game} />
+      <Route path="/buzzer" component={GameBuzzer} />
+      <Route path="/auction" component={GameAuction} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <div className="font-tajawal" dir="rtl">
+          <Toaster />
+          <Router />
+        </div>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
